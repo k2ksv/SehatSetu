22f662231690a00ccd2d158b8c6044687cad6ef4
Himanshu Kumar <your-email@example.com>
2026-05-07 15:06:55 +0530

    Register LabTest and LabTestBooking in Django admin

--- SehatSetu/backend/core/admin.py
+++ SehatSetu/backend/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Hospital, Doctor, Review
+from .models import Hospital, Doctor, Review, LabTest, LabTestBooking
 
 admin.site.register(Hospital)
 admin.site.register(Doctor)
 admin.site.register(Review)
+admin.site.register(LabTest)
+admin.site.register(LabTestBooking)
